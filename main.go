566265be8d68a3aea24334cc8010a67e19568f5a
Nadav Dolin <nadavdolin1@gmail.com>
2022-05-31 14:10:42 +0300

    add current time to main.go
    Signed-off-by: Nadav Dolin <nadavdolin1@gmail.com>

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	currentTime := time.Now()
 	fmt.Println("vim-go")
 	fmt.Println("yeah")
 	fmt.Println("foo")
+	fmt.Println("Current Time: ", currentTime.String())
 }
