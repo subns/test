fc421c8f8d1504ebb003b5f58e47655923494efe
mz201196 <mz201196@Tokyo.com>
2022-05-31 11:32:22 +0300

    added functionality to set 'user to greet' as input
    Signed-off-by: mz201196 <mz201196@Tokyo.com>

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("vim-go")
 	fmt.Println("yeah")
 	fmt.Println("foo")
+
+	var userPtr string
+	flag.StringVar(&userPtr, "user", "", "user to greet")
+	flag.Parse()
 }
